7cc25bae37d1f6545d667aeebb9baba512c4bddd
Michael Joseph <michaeljoseph+github@gmail.com>
2014-12-08 15:46:44 +0200

    More module name references

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 import re
 from setuptools import setup
 
-init_py = open('changes-test/__init__.py').read()
+init_py = open('changes_test/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
 metadata['doc'] = re.findall('"""(.+)"""', init_py)[0]
 
 setup(
-    name='changes-test',
+    name='changes_test',
     version=metadata['version'],
     description=metadata['doc'],
     author=metadata['author'],
     author_email=metadata['email'],
     url=metadata['url'],
-    packages=['changes-test'],
+    packages=['changes_test'],
     include_package_data=True,
     install_requires=[
         'click < 2.1.0'
     ],
     entry_points={
         'console_scripts': [
-            'changes-test = changes-test.cli:main',
+            'changes_test = changes_test.cli:main',
         ],
     },
     test_suite='nose.collector',
